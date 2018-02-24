5a89c96258e288969a778de4786f2cd8a3c077d2
CurtesMalteser <antonio_bastiao@hotmail.com>
2018-02-24 01:08:33 +0100

    TabLayout and new POJO's for Retrofit

---
+++ app/src/main/java/com/curtesmalteser/popularmoviesstage1/utils/ReviewsModel.java
@@ -0,0 +1,9 @@
+package com.curtesmalteser.popularmoviesstage1.utils;
+
+/**
+ * Created by António "Curtes Malteser" Bastião on 24/02/2018.
+ */
+
+
+public class ReviewsModel {
+}
---
+++ app/src/main/java/com/curtesmalteser/popularmoviesstage1/utils/VideosModel.java
@@ -0,0 +1,50 @@
+package com.curtesmalteser.popularmoviesstage1.utils;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by António "Curtes Malteser" Bastião on 23/02/2018.
+ */
+
+
+public class VideosModel {
+
+    @SerializedName("results")
+    @Expose
+    private List<VideosModel> videosModels;
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("key")
+    @Expose
+    private String key;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("type")
+    @Expose
+    private String type;
+
+    public List<VideosModel> getVideosModels() {
+        return videosModels;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
