9b7127ebe5130655e98a565a1e97a02d8a9ba673
CurtesMalteser <antonio_bastiao@hotmail.com>
2021-07-18 22:35:45 +0200

    - converted PopularMoviesFragment.kt into Kotlin with conversion tool (to be optimized) - reverted Java target to version 8 because 11 wasn't compiling - enabled viewBinding

--- app/src/main/java/com/curtesmalteser/popularmoviesstage1/activity/MovieDetailsActivity.kt
+++ app/src/main/java/com/curtesmalteser/popularmoviesstage1/activity/MovieDetailsActivity.kt
@@ -14,93 +14,122 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NavUtils
 import com.curtesmalteser.popularmoviesstage1.R
 import com.curtesmalteser.popularmoviesstage1.adapter.SimpleFragmentPagerAdapter
+import com.curtesmalteser.popularmoviesstage1.databinding.ActivityMovieDetailsBinding
 import com.curtesmalteser.popularmoviesstage1.db.MoviesContract.MoviesEntry
 import com.curtesmalteser.popularmoviesstage1.utils.MoviesModel
 import com.curtesmalteser.popularmoviesstage1.utils.NetworkUtils
 import com.squareup.picasso.Callback
-import com.squareup.picasso.NetworkPolicy
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_movie_details.*
-import kotlinx.android.synthetic.main.details_tablayout.*
-import kotlinx.android.synthetic.main.single_card_view.*
 
 class MovieDetailsActivity : AppCompatActivity() {
 
     private var state = false
 
+    private lateinit var binding: ActivityMovieDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_details)
+
+        binding = ActivityMovieDetailsBinding.inflate(layoutInflater)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        background.setColorFilter(Color.argb(220, 0, 0, 0))
+        binding.background.setColorFilter(Color.argb(220, 0, 0, 0))
 
         if (intent.hasExtra(resources.getString(R.string.string_extra))) {
 
-            intent.getParcelableExtra<MoviesModel>(resources.getString(R.string.string_extra))?.let { model ->
-                getPoster(posterPath = model.posterPath, backdropPath = model.backdropPath)
-                tvTitle.text = model.title
-                voteAverage.text = String.format(getString(R.string.string_vote_average), model.voteAverage)
-                releaseDate.text = model.releaseDate
-                val adapter = SimpleFragmentPagerAdapter(this, supportFragmentManager)
-                viewPager.adapter = adapter
-                tabLayout.setupWithViewPager(viewPager)
+            intent.getParcelableExtra<MoviesModel>(resources.getString(R.string.string_extra))
+                ?.let { model ->
+                    getPoster(posterPath = model.posterPath, backdropPath = model.backdropPath)
+                    binding.tvTitle.text = model.title
+                    binding.voteAverage.text =
+                        String.format(getString(R.string.string_vote_average), model.voteAverage)
+                    binding.releaseDate.text = model.releaseDate
+                    val adapter = SimpleFragmentPagerAdapter(this, supportFragmentManager)
 
 
-                likeButton.setOnClickListener {
-                    if (!state) {
-                        val movieSaved = addFavoriteMovie(model).toLong()
-                        if (movieSaved > 0) {
-                            likeButton.setImageResource(R.drawable.ic_heart_red)
-                            tvAddRemove.setText(R.string.string_remove_fav)
-                            Toast.makeText(this@MovieDetailsActivity, "You like this movie!", Toast.LENGTH_SHORT).show()
-                            state = true
+                    binding.scrollOverviewAndTrailers.let {
+                        it.viewPager.adapter = adapter
+                        it.tabLayout.setupWithViewPager(it.viewPager)
+                    }
+
+
+                    binding.likeButton.setOnClickListener {
+                        if (!state) {
+                            val movieSaved = addFavoriteMovie(model).toLong()
+                            if (movieSaved > 0) {
+                                binding.likeButton.setImageResource(R.drawable.ic_heart_red)
+                                binding.tvAddRemove.setText(R.string.string_remove_fav)
+                                Toast.makeText(
+                                    this@MovieDetailsActivity,
+                                    "You like this movie!",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                state = true
+                            } else {
+                                Toast.makeText(
+                                    this@MovieDetailsActivity,
+                                    R.string.string_error_add_fav,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
                         } else {
-                            Toast.makeText(this@MovieDetailsActivity, R.string.string_error_add_fav, Toast.LENGTH_SHORT).show()
-                        }
-                    } else {
-                        val deleted = removeFavoriteMovie(model)
-                        if (deleted > 0) {
-                            likeButton.setImageResource(R.drawable.ic_heart_white)
-                            tvAddRemove.setText(R.string.string_add_fav)
-                            Toast.makeText(this@MovieDetailsActivity, "You don't like this movie!", Toast.LENGTH_SHORT).show()
-                            state = false
+                            val deleted = removeFavoriteMovie(model)
+                            if (deleted > 0) {
+                                binding.likeButton.setImageResource(R.drawable.ic_heart_white)
+                                binding.tvAddRemove.setText(R.string.string_add_fav)
+                                Toast.makeText(
+                                    this@MovieDetailsActivity,
+                                    "You don't like this movie!",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                state = false
+                            }
                         }
                     }
-                }
-                val cursor = checkFavoriteMovie(model)
-                state = if (cursor!!.count > 0) {
-                    tvAddRemove.setText(R.string.string_remove_fav)
-                    likeButton.setImageResource(R.drawable.ic_heart_red)
-                    true
-                } else {
-                    tvAddRemove.setText(R.string.string_add_fav)
-                    likeButton.setImageResource(R.drawable.ic_heart_white)
-                    false
-                }
+                    val cursor = checkFavoriteMovie(model)
+                    state = if (cursor!!.count > 0) {
+                        binding.tvAddRemove.setText(R.string.string_remove_fav)
+                        binding.likeButton.setImageResource(R.drawable.ic_heart_red)
+                        true
+                    } else {
+                        binding.tvAddRemove.setText(R.string.string_add_fav)
+                        binding.likeButton.setImageResource(R.drawable.ic_heart_white)
+                        false
+                    }
 
-                cursor.close()
-            }
+                    cursor.close()
+                }
         }
     }
 
     private fun getPoster(posterPath: String, backdropPath: String) {
         Picasso.get().let {
             it.loadImage(
-                    uri = NetworkUtils.getPosterUrl(resources.getString(R.string.poster_width_segment), posterPath),
-                    imageView = posterInDetailsActivity,
-                    success = {
-                        getBackground(NetworkUtils.getPosterUrl(resources.getString(R.string.poster_width_segment), backdropPath))
-                    },
-                    error = {
-                        it.loadImage(uri = NetworkUtils.getPosterUrl(resources.getString(R.string.poster_width_segment), posterPath),
-                                imageView = posterInDetailsActivity,
-                                errorResId = R.drawable.ic_heart_white,
-                                success = {},
-                                error = { Log.e("Picasso", "Could not fetch image") }
+                uri = NetworkUtils.getPosterUrl(
+                    resources.getString(R.string.poster_width_segment),
+                    posterPath
+                ),
+                imageView = binding.posterInDetailsActivity,
+                success = {
+                    getBackground(
+                        NetworkUtils.getPosterUrl(
+                            resources.getString(R.string.poster_width_segment),
+                            backdropPath
                         )
-                    }
+                    )
+                },
+                error = {
+                    it.loadImage(uri = NetworkUtils.getPosterUrl(
+                        resources.getString(R.string.poster_width_segment),
+                        posterPath
+                    ),
+                        imageView = binding.posterInDetailsActivity,
+                        errorResId = R.drawable.ic_heart_white,
+                        success = {},
+                        error = { Log.e("Picasso", "Could not fetch image") }
+                    )
+                }
             )
         }
     }
@@ -108,30 +137,32 @@ class MovieDetailsActivity : AppCompatActivity() {
     private fun getBackground(backdropPath: Uri) {
         Picasso.get().let {
             it.loadImage(
-                    uri = backdropPath,
-                    imageView = background,
-                    success = {},
-                    error = {
-                        it.loadImage(uri = backdropPath,
-                                imageView = background,
-                                success = {},
-                                error = { Log.e("Picasso", "Could not fetch image") }
-                        )
-                    }
+                uri = backdropPath,
+                imageView = binding.background,
+                success = {},
+                error = {
+                    it.loadImage(uri = backdropPath,
+                        imageView = binding.background,
+                        success = {},
+                        error = { Log.e("Picasso", "Could not fetch image") }
+                    )
+                }
             )
         }
     }
 
-    private fun Picasso.loadImage(uri: Uri,
-                                  imageView: ImageView,
-                                  success: () -> Unit,
-                                  error: () -> Unit,
-                                  @DrawableRes errorResId: Int = R.drawable.drawable_background) = load(uri)
-            .error(errorResId)
-            .into(imageView, object : Callback {
-                override fun onSuccess(): Unit = success()
-                override fun onError(e: Exception?): Unit = error()
-            })
+    private fun Picasso.loadImage(
+        uri: Uri,
+        imageView: ImageView,
+        success: () -> Unit,
+        error: () -> Unit,
+        @DrawableRes errorResId: Int = R.drawable.drawable_background
+    ) = load(uri)
+        .error(errorResId)
+        .into(imageView, object : Callback {
+            override fun onSuccess(): Unit = success()
+            override fun onError(e: Exception?): Unit = error()
+        })
 
 
     private fun addFavoriteMovie(model: MoviesModel): Int {
@@ -150,18 +181,18 @@ class MovieDetailsActivity : AppCompatActivity() {
     }
 
     private fun removeFavoriteMovie(model: MoviesModel): Int = MoviesEntry.CONTENT_URI
-            .buildUpon()
-            .appendPath(model.id.toString())
-            .build().run {
-                contentResolver.delete(this, null, null)
-            }
+        .buildUpon()
+        .appendPath(model.id.toString())
+        .build().run {
+            contentResolver.delete(this, null, null)
+        }
 
     private fun checkFavoriteMovie(model: MoviesModel): Cursor? = contentResolver.query(
-            MoviesEntry.CONTENT_URI,
-            null,
-            MoviesEntry.COLUMN_NAME_ID + " = ? ",
-            arrayOf(model.id.toString()),
-            null
+        MoviesEntry.CONTENT_URI,
+        null,
+        MoviesEntry.COLUMN_NAME_ID + " = ? ",
+        arrayOf(model.id.toString()),
+        null
     )
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
---
+++ app/src/main/java/com/curtesmalteser/popularmoviesstage1/fragment/PopularMoviesFragment.kt
@@ -0,0 +1,125 @@
+package com.curtesmalteser.popularmoviesstage1.fragment
+
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.os.Bundle
+import android.os.Parcelable
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.curtesmalteser.popularmoviesstage1.BuildConfig
+import com.curtesmalteser.popularmoviesstage1.R
+import com.curtesmalteser.popularmoviesstage1.activity.MovieDetailsActivity
+import com.curtesmalteser.popularmoviesstage1.adapter.EndlessScrollListener
+import com.curtesmalteser.popularmoviesstage1.adapter.MoviesAdapter
+import com.curtesmalteser.popularmoviesstage1.utils.MoviesAPIClient
+import com.curtesmalteser.popularmoviesstage1.utils.MoviesAPIInterface
+import com.curtesmalteser.popularmoviesstage1.utils.MoviesModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.*
+
+class PopularMoviesFragment : Fragment(), MoviesAdapter.ListItemClickListener {
+    private var mMoviesList: ArrayList<MoviesModel>? = ArrayList()
+    private var stateRecyclerView: Parcelable? = null
+    private var pageNumber = 1
+    private var cm: ConnectivityManager? = null
+    private var moviesAdapter: MoviesAdapter? = null
+
+    private lateinit var mRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+        cm =requireActivity().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_movies_layout, container, false)
+
+        mRecyclerView = view.findViewById(R.id.recyclerView)
+        mRecyclerView.setHasFixedSize(true)
+        val layoutManager =
+            GridLayoutManager(context, resources.getInteger(R.integer.number_of_columns))
+        mRecyclerView.layoutManager = layoutManager
+
+        if (savedInstanceState == null) {
+            makeMoviesQuery(pageNumber)
+        } else {
+            mMoviesList = savedInstanceState.getParcelableArrayList(SAVED_STATE_MOVIES_LIST)
+            pageNumber = savedInstanceState.getInt(PAGE_NUMBER_KEY)
+            mRecyclerView.layoutManager?.onRestoreInstanceState(stateRecyclerView)
+        }
+        moviesAdapter = MoviesAdapter(context, mMoviesList, this)
+        mRecyclerView.adapter = moviesAdapter
+        mRecyclerView.addOnScrollListener(object : EndlessScrollListener(layoutManager) {
+            override fun onLoadMore(current_page: Int) {
+                makeMoviesQuery(current_page)
+                pageNumber = current_page
+            }
+        })
+        return view
+    }
+
+    private fun makeMoviesQuery(page: Int) {
+        val apiInterface = MoviesAPIClient.getClient().create(
+            MoviesAPIInterface::class.java
+        )
+        val call: Call<MoviesModel>
+        if (cm!!.activeNetworkInfo != null && cm!!.activeNetworkInfo!!.isAvailable
+            && cm!!.activeNetworkInfo!!.isConnected
+        ) {
+            val queryParams: MutableMap<String, String> = HashMap()
+            queryParams["api_key"] = BuildConfig.API_KEY
+            queryParams["language"] = "en-US"
+            queryParams["page"] = page.toString()
+            call = apiInterface.getPopularMovies(queryParams)
+            call.enqueue(object : Callback<MoviesModel> {
+                override fun onResponse(call: Call<MoviesModel>, response: Response<MoviesModel>) {
+                    Log.d(TAG, "onResponse: " + response.raw())
+                    for (moviesModel in response.body()!!.moviesModels) {
+                        mMoviesList!!.add(moviesModel)
+                        moviesAdapter!!.notifyDataSetChanged()
+                    }
+                }
+
+                override fun onFailure(call: Call<MoviesModel>, t: Throwable) {
+                    Log.d(TAG, "onFailure:" + t.message)
+                }
+            })
+        } else Toast.makeText(context, R.string.check_internet_connection, Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    override fun onListItemClick(moviesModel: MoviesModel) {
+        val intent = Intent(activity, MovieDetailsActivity::class.java)
+        intent.putExtra(resources.getString(R.string.string_extra), moviesModel)
+        startActivity(intent)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        stateRecyclerView = mRecyclerView.layoutManager!!.onSaveInstanceState()
+        outState.putInt(PAGE_NUMBER_KEY, pageNumber)
+        outState.putParcelableArrayList(SAVED_STATE_MOVIES_LIST, mMoviesList)
+    }
+
+    companion object {
+        private val TAG = PopularMoviesFragment::class.java.simpleName
+        private const val SAVED_STATE_MOVIES_LIST = "moviesListSaved"
+        private const val PAGE_NUMBER_KEY = "pageNumber"
+        fun newInstance(): PopularMoviesFragment {
+            return PopularMoviesFragment()
+        }
+    }
+}
