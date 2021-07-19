82c6848d81c69b45163da441aa6259dcf4b43580
CurtesMalteser <antonio_bastiao@hotmail.com>
2021-07-19 22:15:24 +0200

    added App.kt

---
+++ app/src/main/java/com/curtesmalteser/popularmoviesstage1/app/App.kt
@@ -0,0 +1,9 @@
+package com.curtesmalteser.popularmoviesstage1.app
+
+import android.app.Application
+
+/**
+ * Created by António 'Curtes Malteser' Bastião on 19/07/2021.
+ */
+class App : Application() {
+}
