698af7cc5fed0f79c05a5cec06cec992f96272ea
Curtes Malteser <antonio_bastiao@hotmail.com>
2023-10-18 22:32:04 +0200

    moves MoviesAPIInterface.kt

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -37,6 +37,10 @@ dependencies {
     implementation(project(":core"))
     implementation("com.google.code.gson:gson:2.8.9")
 
+    val retrofitDependencies = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitDependencies")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitDependencies")
+
     testImplementation("junit:junit:4.13.2")
 
 }
