3d675cb5ffa4fcceb5324b280b7347dad15d40ce
Curtes Malteser <antonio_bastiao@hotmail.com>
2023-10-18 20:44:13 +0200

    adds core module

---
+++ core/src/main/java/com/curtesmalteser/core/models/Movie.kt
@@ -0,0 +1,10 @@
+package com.curtesmalteser.core.models
+
+/**
+ * Created by António Bastião on 18.10.2023
+ * Refer to <a href="https://github.com/CurtesMalteser">CurtesMalteser github</a>
+ */
+interface Movie {
+    val title: String
+    val posterPath: String
+}
