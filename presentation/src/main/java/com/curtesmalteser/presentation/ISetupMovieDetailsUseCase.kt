9a818a6d78e0e315b0e5608678e1a50b31eedb27
Curtes Malteser <antonio_bastiao@hotmail.com>
2023-11-07 10:58:25 +0100

    renames IMovieDetailsUseCase as ISetupMovieDetailsUseCase

---
+++ presentation/src/main/java/com/curtesmalteser/presentation/ISetupMovieDetailsUseCase.kt
@@ -0,0 +1,34 @@
+package com.curtesmalteser.presentation
+
+import com.curtesmalteser.popularmovies.core.di.PopularMoviesRepo
+import com.curtesmalteser.popularmovies.core.di.TopRatedRepo
+import com.curtesmalteser.popularmovies.repository.IMoviesRepository
+import com.curtesmalteser.popularmovies.repository.details.IMovieDetailsRepository
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.merge
+
+/**
+ * Created by António Bastião on 22.10.2023
+ * Refer to <a href="https://github.com/CurtesMalteser">CurtesMalteser github</a>
+ */
+interface ISetupMovieDetailsUseCase {
+    suspend fun getMovieDetails(movieId: Long)
+}
+
+internal class SetupMovieDetailsUseCase(
+    @PopularMoviesRepo private val popularMoviesRepository: IMoviesRepository,
+    @TopRatedRepo private val topMoviesRepository: IMoviesRepository,
+    private val movieDetailsRepository: IMovieDetailsRepository,
+) : ISetupMovieDetailsUseCase {
+
+    override suspend fun getMovieDetails(movieId: Long) {
+        merge(
+            popularMoviesRepository.moviesList,
+            topMoviesRepository.moviesList,
+        ).map { listOfMoviesData ->
+            listOfMoviesData.find { movieData -> movieData.id == movieId }
+        }.map { details ->
+            movieDetailsRepository.setupMovie(details)
+        }
+    }
+}
---
+++ presentation/src/test/java/com/curtesmalteser/presentation/SetupMovieDetailsUseCaseTest.kt
@@ -0,0 +1,16 @@
+package com.curtesmalteser.presentation
+
+import io.kotest.matchers.ints.shouldBeExactly
+import org.junit.jupiter.api.Test
+
+/**
+ * Created by António Bastião on 06.11.2023
+ * Refer to [CurtesMalteser github](https://github.com/CurtesMalteser)
+ */
+class SetupMovieDetailsUseCaseTest {
+
+    @Test
+    fun getMovieDetails() {
+        4 shouldBeExactly 2+2
+    }
+}
