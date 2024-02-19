c0e05c45f098088c860845740d19acbf557b2d02
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-02-19 17:29:38 +0100

    sets BackgroundImage

--- app/src/main/java/com/curtesmalteser/popularmoviesstage1/screen/details/MovieDetailsScreen.kt
+++ app/src/main/java/com/curtesmalteser/popularmoviesstage1/screen/details/MovieDetailsScreen.kt
@@ -1,11 +1,25 @@
 package com.curtesmalteser.popularmoviesstage1.screen.details
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.SrcAtop
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.curtesmalteser.popularmovies.repository.details.MovieDetailsResult
+import com.curtesmalteser.popularmoviesstage1.R
+import com.curtesmalteser.popularmoviesstage1.utils.NetworkUtils.getPosterUrl
 
 
 /**
@@ -28,7 +42,15 @@ fun MovieDetailsScreen(
     movieDetails.fold(
         onSuccess = { movieDetailsResult ->
             when (movieDetailsResult) {
-                is MovieDetailsResult.MovieDetailsData ->  Text(text = movieDetailsResult.title)
+                is MovieDetailsResult.MovieDetailsData -> {
+                    BackgroundImage(backdropPath = movieDetailsResult.backdropPath) {
+                        Text(
+                            text = movieDetailsResult.title,
+                            color = Color.White,
+                            fontSize = 24.sp,
+                        )
+                    }
+                }
                 MovieDetailsResult.NoDetails ->  Text(text = movieDetailsResult.toString())
             }
 
@@ -37,4 +59,24 @@ fun MovieDetailsScreen(
             Text(text = error.message ?: "Unknown error")
         }
     )
+}
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun BackgroundImage(
+    backdropPath: String,
+    content: @Composable BoxScope.() -> Unit,
+) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        GlideImage(
+            model = getPosterUrl(stringResource(R.string.poster_width_segment), backdropPath),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.matchParentSize(),
+            colorFilter = ColorFilter.tint(Color.Black.copy(alpha = 0.9f), SrcAtop),
+        )
+        content()
+    }
 }
