630ff54ac49a3f9411fc028fcebccef0703acd6b
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-10-19 17:56:45 +0200

    feat: Improve VideosRow UI by showing the Thumbnail and the title

--- app/src/main/java/com/curtesmalteser/popularmoviesstage1/component/details/VideosRow.kt
+++ app/src/main/java/com/curtesmalteser/popularmoviesstage1/component/details/VideosRow.kt
@@ -1,22 +1,31 @@
 package com.curtesmalteser.popularmoviesstage1.component.details
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.curtesmalteser.popularmovies.data.VideosModelData
 import com.curtesmalteser.popularmoviesstage1.R
 import com.curtesmalteser.popularmoviesstage1.component.CollapsibleRow
+import com.curtesmalteser.popularmoviesstage1.utils.NetworkUtils.getThumbnail
 
 /**
  * Created by António Bastião on 12.04.2024
  * Refer to <a href="https://github.com/CurtesMalteser">CurtesMalteser github</a>
  */
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun VideosRow(videosResult: Result<VideosModelData>) {
     CollapsibleRow(title = stringResource(id = R.string.string_videos)) {
@@ -24,18 +33,26 @@ fun VideosRow(videosResult: Result<VideosModelData>) {
             onSuccess = { reviews ->
                 Column {
                     reviews.videosModels.map {
-                        Text(
-                            text = it.name,
-                            color = Color.White,
-                        )
-                        Text(
-                            text = it.type,
-                            color = Color.White,
-                        )
-                        Text(
-                            text = it.key,
-                            color = Color.White,
-                        )
+                        DetailsCard {
+                            Row {
+                                GlideImage(
+                                    model = getThumbnail(it.key),
+                                    contentDescription = null,
+                                    contentScale = ContentScale.Inside,
+                                    modifier = Modifier
+                                        .fillMaxWidth(.40f)
+                                        .padding(8.dp)
+                                        .clip(RoundedCornerShape(8.dp)),
+                                )
+
+                                Text(
+                                    text = it.name,
+                                    color = Color.White,
+                                    modifier = Modifier.padding(8.dp),
+                                )
+
+                            }
+                        }
                     }
                 }
 
